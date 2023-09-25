a417646806e35e4213655d9bb05d69c660c94aba
Tiare Infante <tiare.infante.snmartin@gmail.com>
2023-09-25 11:44:36 -0300

    filtros

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,34 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterby, value) => {
+  return data.filter((item) => {
+    return item[filterby] === value;
+  });
 };
 
-export const anotherExample = () => {
-  return [];
+//funcion de ordenar asc y desc
+export const sortData = (data, sortBy, sortOrder) => {
+//verificar si sortorder es asc o desc
+  if (sortOrder === "asc") {
+    data.sort((a,b) => {
+      if (a[sortBy] < b[sortBy]) {
+        return -1;
+      }
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sortOrder=== "desc") {
+    data.sort((a,b) => {
+      if(a[sortBy] > b[sortBy]){
+        return -1;
+      }
+      if (a[sortBy] < b[sortBy]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  return data;
 };
